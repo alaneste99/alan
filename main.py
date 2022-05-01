cd61690879d6929c16d8a18236f70d3ce78b3bc4
alaneste99 <104707276+alaneste99@users.noreply.github.com>
2022-05-01 01:20:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -241,7 +241,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'AQUI USUARIO ADMIN DEL BOT SIN @'
+        tl_admin_user = 'https://t.me/alian39'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
